48ab43cd54711547c324d6b617b7b2543f5ef71c
JoseRoj <jorojas2019@udec.cl>
2023-10-25 20:33:21 -0300

    Vista registros V1

---
+++ app/lib/pages/register.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:app/pages/widget.dart';
+import 'package:app/utils/colors.dart';
+
+class tableRegister extends StatefulWidget {
+  const tableRegister({super.key});
+
+  @override
+  State<tableRegister> createState() => _tableRegisterState();
+}
+
+class _tableRegisterState extends State<tableRegister> {
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, Object>> args =
+        ModalRoute.of(context)!.settings.arguments as List<Map<String, Object>>;
+    return Scaffold(
+      backgroundColor: colorBackground,
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.person, color: Colors.white),
+          ),
+        ],
+        title: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "Registros",
+            textAlign: TextAlign.left,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      //_appbar[_currentIndex],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 00.0, left: 20.0, right: 20.0),
+        child: SingleChildScrollView(
+          child: Table(
+            border: TableBorder.all(color: Colors.black),
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            children: [
+              TableRow(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                ),
+                children: [
+                  TableCell(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Ingreso"),
+                    ),
+                  ),
+                  TableCell(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Salida"),
+                    ),
+                  ),
+                ],
+              ),
+              ...List.generate(
+                20,
+                (index) => TableRow(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  children: [
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("${args[1]['title']}"),
+                      ),
+                    ),
+                    TableCell(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("${args[0]['title']}"),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ), /*AppBar(
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.person, color: Colors.white),
+          ),
+        ],
+        title: const Text(
+          'Acceso',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),*/
+    );
+  }
+}
