48ed64e947fd8d093352f7e75ad1cf6451209761
JoseRoj <jorojas2019@udec.cl>
2023-11-21 01:41:58 -0300

    feature: Add cambio de contraseña

--- app/lib/pages/perfil.dart
+++ app/lib/pages/perfil.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
-
 import 'package:app/token/accces_token-dart.dart';
 import 'package:flutter/material.dart';
 import 'package:app/utils/colors.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 
 class perfil extends StatefulWidget {
@@ -15,13 +15,79 @@ class perfil extends StatefulWidget {
 }
 
 class _perfilState extends State<perfil> {
-  TextEditingController password = TextEditingController();
+  bool _mostrarIndicadorCarga = false;
+
   bool _obscureTextone = true;
   bool _obscureTexttwo = true;
   Auth auth = Auth();
-  TextEditingController repassword = TextEditingController();
+  TextEditingController current_password = TextEditingController();
+  TextEditingController new_password = TextEditingController();
+  TextEditingController rep_new_password = TextEditingController();
 
   bool editar = false;
+
+  Future<void> changePassword() async {
+    try {
+      final response = await http.post(
+        Uri.parse('http://${dotenv.env['BASE_URL']}:5000/user/change-password'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer ${auth.token}',
+        },
+        body: jsonEncode(<String, String>{
+          "current_password": current_password.text,
+          "new_password": new_password.text
+        }),
+      );
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      if (response.statusCode == 200) {
+        Fluttertoast.showToast(
+            msg: responseData['message'],
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.TOP,
+            timeInSecForIosWeb: 2,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      } else {
+        Fluttertoast.showToast(
+            msg: responseData['error'],
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.TOP,
+            timeInSecForIosWeb: 2,
+            backgroundColor: Colors.red,
+            textColor: Colors.red,
+            fontSize: 12.0);
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Widget sectionPassword(TextEditingController controller, String label) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.7,
+      height: 60,
+      child: TextFormField(
+        controller: controller,
+        obscureText: _obscureTextone,
+        decoration: InputDecoration(
+          suffixIcon: IconButton(
+            icon: Icon(
+              _obscureTextone ? Icons.visibility : Icons.visibility_off,
+            ),
+            onPressed: () {
+              setState(() {
+                _obscureTextone =
+                    !_obscureTextone; // Cambia entre texto visible y oculto
+              });
+            },
+          ),
+          labelText: "Contraseña Actual",
+        ),
+      ),
+    );
+  }
 
   Widget updatePassword(BuildContext context) {
     return Padding(
@@ -29,50 +95,9 @@ class _perfilState extends State<perfil> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.7,
-            height: 60,
-            child: TextFormField(
-              controller: password,
-              obscureText: _obscureTextone,
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscureTextone ? Icons.visibility : Icons.visibility_off,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _obscureTextone =
-                          !_obscureTextone; // Cambia entre texto visible y oculto
-                    });
-                  },
-                ),
-                labelText: "Contraseña",
-              ),
-            ),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.7,
-            height: 60,
-            child: TextFormField(
-              controller: repassword,
-              obscureText: _obscureTexttwo,
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscureTexttwo ? Icons.visibility : Icons.visibility_off,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _obscureTexttwo =
-                          !_obscureTexttwo; // Cambia entre texto visible y oculto
-                    });
-                  },
-                ),
-                labelText: "Repetir Contraseña",
-              ),
-            ),
-          ),
+          sectionPassword(current_password, "Contraseña Actual"),
+          sectionPassword(new_password, "Nueva Contraseña"),
+          sectionPassword(rep_new_password, "Repetir Nueva Contraseña"),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10),
             child: ElevatedButton(
@@ -82,18 +107,25 @@ class _perfilState extends State<perfil> {
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
-              onPressed: () {
-                if (password.text != repassword.text) {
+              onPressed: () async {
+                print("new password: ${new_password.text}");
+                print("new password: ${rep_new_password.text}");
+
+                if (new_password.text != rep_new_password.text) {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
                         content: Text('Las contraseñas no coinciden')),
                   );
-                } else if (password.text.isEmpty || repassword.text.isEmpty) {
+                } else if (current_password.text.isEmpty ||
+                    new_password.text.isEmpty) {
                   ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Campos vacios')));
+                      const SnackBar(content: Text('Existen campos vacios')));
                   // Validate returns true if the form is valid, or false otherwise.
                 } else {
-                  //* REALIZAR LLAMADA A API PARA CAMBIAR CONTRASEÑA
+                  await changePassword();
+                  await logout();
+                  await auth.clearToken();
+                  Navigator.pushNamed(context, '/login');
                 }
               },
               child: const Text('Cambiar'),
@@ -105,18 +137,49 @@ class _perfilState extends State<perfil> {
   }
 
   Future<void> logout() async {
-    print("token logout: ${auth.token}");
-    final response = await http.post(
-      Uri.parse('http://${dotenv.env['BASE_URL']}:5000/user/logout'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization': 'Bearer ${auth.token}',
+    setState(
+      () {
+        _mostrarIndicadorCarga = true;
       },
     );
-    if (response.statusCode == 200) {
-      Center(child: CircularProgressIndicator());
-    } else {
+    print("token logout: ${auth.token}");
+    try {
+      final response = await http.post(
+        Uri.parse('http://${dotenv.env['BASE_URL']}:5000/user/logout'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer ${auth.token}',
+        },
+      );
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      if (response.statusCode == 200) {
+        Fluttertoast.showToast(
+            msg: responseData['msg'],
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.TOP,
+            timeInSecForIosWeb: 2,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      } else {
+        Fluttertoast.showToast(
+            msg: "Error al cerrar sesión",
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.TOP,
+            timeInSecForIosWeb: 2,
+            backgroundColor: Colors.red,
+            textColor: Colors.red,
+            fontSize: 12.0);
+      }
+    } catch (e) {
       throw Exception('Error Al Cerrar Sesión');
+    } finally {
+      // Oculta el indicador de carga después de la solicitud
+      setState(
+        () {
+          _mostrarIndicadorCarga = false;
+        },
+      );
     }
   }
 
@@ -142,115 +205,121 @@ class _perfilState extends State<perfil> {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(left: 10, right: 20, top: 20),
-              child: const CircleAvatar(
-                maxRadius: 100,
-                //backgroundImage: NetworkImage(""), // insertar imagen de perfil
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 10, right: 20, top: 20),
+                child: const CircleAvatar(
+                  maxRadius: 100,
+                  //backgroundImage: NetworkImage(""), // insertar imagen de perfil
+                ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              child: const Text(
-                "Micheal Scott",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: const Text(
+                  "Micheal Scott",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            Container(
-              child: const Text(
-                "administrador",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              Container(
+                child: const Text(
+                  "Administrador",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 10),
-              alignment: Alignment.centerLeft,
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 35,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
+              Container(
+                padding: EdgeInsets.only(left: 10),
+                alignment: Alignment.centerLeft,
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 35,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                margin: const EdgeInsets.only(top: 20),
+                child: const Text(
+                  "Correo",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
               ),
-              margin: const EdgeInsets.only(top: 20),
-              child: const Text(
-                "Correo",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              Container(
+                padding: EdgeInsets.only(left: 10),
+                alignment: Alignment.centerLeft,
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 35,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                margin: const EdgeInsets.only(top: 5),
+                child: const Text(
+                  "Correo",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 10),
-              alignment: Alignment.centerLeft,
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 35,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
+              Container(
+                padding: EdgeInsets.only(left: 10),
+                alignment: Alignment.centerLeft,
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 35,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                margin: const EdgeInsets.only(top: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Cambiar contraseña",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (editar == true) {
+                          editar = false;
+                        } else {
+                          editar = true;
+                        }
+                        setState(() {});
+                      },
+                      child: !editar
+                          ? Icon(Icons.edit)
+                          : Icon(Icons.arrow_drop_up),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          disabledBackgroundColor: Colors.white,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                  ],
+                ),
               ),
-              margin: const EdgeInsets.only(top: 5),
-              child: const Text(
-                "Correo",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              editar ? updatePassword(context) : const Text(""),
+              ElevatedButton(
+                onPressed: () async {
+                  await logout();
+                  await auth.clearToken();
+                  Navigator.pushNamed(context, '/login');
+                },
+                child: const Text("Cerrar Sesión",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 10),
-              alignment: Alignment.centerLeft,
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 35,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              margin: const EdgeInsets.only(top: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Cambiar contraseña",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (editar == true) {
-                        editar = false;
-                      } else {
-                        editar = true;
-                      }
-                      setState(() {});
-                    },
-                    child:
-                        !editar ? Icon(Icons.edit) : Icon(Icons.arrow_drop_up),
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        disabledBackgroundColor: Colors.white,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                  ),
-                ],
-              ),
-            ),
-            editar ? updatePassword(context) : const Text(""),
-            ElevatedButton(
-              onPressed: () async {
-                await logout();
-                await auth.clearToken();
-                Navigator.pushNamed(context, '/login');
-              },
-              child: const Text("Cerrar Sesión",
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white)),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10))),
-            ),
-          ],
+              if (_mostrarIndicadorCarga) const CircularProgressIndicator()
+            ],
+          ),
         ),
       ),
     );
