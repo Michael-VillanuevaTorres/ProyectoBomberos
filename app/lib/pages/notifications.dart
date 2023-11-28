5bef5d86cced72515fcbc5483a00e83c2a2fb801
JoseRoj <jorojas2019@udec.cl>
2023-11-28 12:09:59 -0300

    Agregando sistema de notifaciones

--- app/lib/pages/notifications.dart
+++ app/lib/pages/notifications.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
 import 'dart:ui';
+import 'package:app/object/bitacora.dart';
+import 'package:app/pages/widget.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 import 'package:app/utils/colors.dart';
@@ -14,24 +18,79 @@ class notification extends StatefulWidget {
 
 class _notificationState extends State<notification> {
   late List<dynamic> notifications;
-  get http => null;
+  late Future<void> _initLoad;
 
   Future<void> getNotificaciones() async {
     try {
       final response = await http.get(Uri.parse(
-          "http://${dotenv.env['BASE_URL']}:5000/user/users_by_state/1"));
-      if (response.statusCode == 200) {}
+          "http://${dotenv.env['BASE_URL']}:5000/logs?description=0"));
+      notifications = json.decode(response.body);
 
-      //return list.where((element) => element.state == value).toList();
+      if (response.statusCode == 200) {
+        notifications =
+            notifications.map((bit) => Bitacora.fromJson(bit)).toList();
+        notifications = notifications.reversed.toList();
+      }
     } catch (error) {
       // Manejo de errores de la solicitud
       throw Exception('Failed to load User');
     }
   }
 
+  Widget viewNotification(String description, String date) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10, right: 10, left: 10),
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: 80,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0), // Bordes redondeados
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 94, 94, 94)
+                .withOpacity(0.2), // Color de la sombra
+            spreadRadius: 5,
+            blurRadius: 5,
+            offset: Offset(0, 2), // Desplazamiento de la sombra
+          ),
+        ],
+        color: const Color.fromRGBO(255, 255, 255, 40), // Color del contenedor
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                child: Text(description,
+                    overflow: TextOverflow.ellipsis, maxLines: 2),
+              ),
+            ),
+            Container(
+              child: Text(date),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget textAlert(String label, String value) {
+    return Row(
+      children: [
+        Text(
+          label,
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        Text(value),
+      ],
+    );
+  }
+
   @override
   void initState() {
     super.initState();
+    _initLoad = getNotificaciones();
   }
 
   @override
@@ -50,172 +109,113 @@ class _notificationState extends State<notification> {
           ),
         ),
       ),
-      body: Center(
+      body: Container(
         child: Column(
           children: [
-            GestureDetector(
-              onTap: () => showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                    child: AlertDialog(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Autor:",
-                            style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold),
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                "Fecha:",
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                "   ",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.normal),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                "Unidad:",
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                " Unidad 1",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.normal),
-                              ),
-                            ],
-                          ),
-                          Text(
-                            "Observación",
-                            style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold),
-                          ),
-                          Text("dsnfkjsdngjkfdngjkdfg"),
-                        ],
-                      ),
-                    ),
-                  );
+            Expanded(
+              child: FutureBuilder(
+                future: _initLoad,
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.done:
+                      {
+                        return RefreshIndicator(
+                            onRefresh: () => getNotificaciones(),
+                            child: ListView.builder(
+                                itemCount: notifications.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  final item = notifications[index];
+                                  return GestureDetector(
+                                    onTap: () => showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return BackdropFilter(
+                                          filter: ImageFilter.blur(
+                                              sigmaX: 5, sigmaY: 5),
+                                          child: AlertDialog(
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(12.0),
+                                            ),
+                                            backgroundColor:
+                                                const Color.fromRGBO(
+                                                    255, 255, 255, 1),
+                                            content: Column(
+                                              mainAxisSize: MainAxisSize.min,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                textAlert("Autor: ", item.user),
+                                                textAlert(
+                                                    "Fecha: ", item.dateTime),
+                                                textAlert("Unidad: ",
+                                                    item.truckPatent),
+                                                item.type == "Single_use"
+                                                    ? textAlert(
+                                                        "Tipo: ", "Uso común")
+                                                    : item.type == "Maintenance"
+                                                        ? textAlert("Tipo: ",
+                                                            "Mantención")
+                                                        : textAlert("Tipo: ",
+                                                            "Reparación"),
+                                                textAlert(
+                                                    "Nivel de combustible: ",
+                                                    transformTodouble(item
+                                                        .fuelLevel
+                                                        .toString())),
+                                                textAlert(
+                                                    "Nivel de aceite: ",
+                                                    transformTodouble(item
+                                                        .waterLevel
+                                                        .toString())),
+                                                textAlert(
+                                                    "Nivel de agua: ",
+                                                    transformTodouble(item
+                                                        .oilLevel
+                                                        .toString())),
+                                                const Text(
+                                                  "Observación:",
+                                                  style: TextStyle(
+                                                      fontSize: 15,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                ),
+                                                Text(item.description),
+                                              ],
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                    child: index == notifications.length - 1
+                                        ? Container(
+                                            margin: const EdgeInsets.only(
+                                                bottom: 10),
+                                            child: viewNotification(
+                                                item.description,
+                                                item.dateTime),
+                                          )
+                                        : viewNotification(
+                                            item.description, item.dateTime),
+                                  );
+                                }));
+                      }
+                    case ConnectionState.waiting:
+                      {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                    case ConnectionState.active:
+                      {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                    case ConnectionState.none:
+                      {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                  }
                 },
               ),
-              child: Container(
-                margin: EdgeInsets.only(top: 10, left: 10),
-                width: MediaQuery.of(context).size.width * 0.9,
-                height: 80,
-                decoration: BoxDecoration(
-                  borderRadius:
-                      BorderRadius.circular(12.0), // Bordes redondeados
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color.fromARGB(255, 94, 94, 94)
-                          .withOpacity(0.2), // Color de la sombra
-                      spreadRadius: 5,
-                      blurRadius: 5,
-                      offset: Offset(0, 2), // Desplazamiento de la sombra
-                    ),
-                  ],
-                  color: const Color.fromRGBO(
-                      255, 255, 255, 40), // Color del contenedor
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          child: Text(
-                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2),
-                        ),
-                      ),
-                      Container(
-                        child: Text("10-20-2023"),
-                      )
-                    ],
-                  ),
-                ),
-              ),
             ),
-            Container(
-              margin: EdgeInsets.only(top: 10, left: 10),
-              width: MediaQuery.of(context).size.width * 0.9,
-              height: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12.0), // Bordes redondeados
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromARGB(255, 94, 94, 94)
-                        .withOpacity(0.2), // Color de la sombra
-                    spreadRadius: 5,
-                    blurRadius: 5,
-                    offset: Offset(0, 2), // Desplazamiento de la sombra
-                  ),
-                ],
-                color: const Color.fromRGBO(
-                    255, 255, 255, 40), // Color del contenedor
-              ),
-              child: Center(
-                child: Text(
-                  'Texto en el Contenedor',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            /*Expanded(child: Container()
-                  /* FutureBuilder(
-                  future: _initLoad,
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.done:
-                        {
-                          return RefreshIndicator(
-                              onRefresh: () => getUser(dropdownValue!),
-                              child: ListView.builder(
-                                  itemCount: users.length,
-                                  itemBuilder: (BuildContext context, int index) {
-                                    final item = users[index];
-                                    return stateBombero(firefighter: item);
-                                  }));
-                        }
-                      case ConnectionState.waiting:
-                        {
-                          return const Center(child: CircularProgressIndicator());
-                        }
-                      case ConnectionState.active:
-                        {
-                          return const Center(child: CircularProgressIndicator());
-                        }
-                      case ConnectionState.none:
-                        {
-                          return const Center(child: CircularProgressIndicator());
-                        }
-                    }
-                  },
-                ),*/
-                  ),*/
           ],
         ),
       ),
